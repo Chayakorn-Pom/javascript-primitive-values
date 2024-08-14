3240f88d73dbb348ffef3e6f3587ce0b216f7735
Pom <chayakon500@gmail.com>
2024-08-14 17:00:34 +0700

    BP5-POM ex1-8

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000
+let discountPrice = (totalPrice/100) *25
+let finalPrice = totalPrice - discountPrice
+console.log(finalPrice) 
